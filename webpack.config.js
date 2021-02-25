e9458abe6f3dec6a937d622a0566ddd3a3ca90ad
iainbrux <me@iainbrux.com>
2021-02-25 00:42:22 +0000

    Initial files upload

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { webpack } = require("webpack");
+
+module.exports = {
+  entry: "./app/index.js",
+  module: {
+    rules: [
+      { test: /\.svg$/, use: "svg-inline-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(js)$/, use: "babel-loader" },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "app/index.html" })],
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+};
