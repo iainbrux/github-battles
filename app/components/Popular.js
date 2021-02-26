ed75fda3c58566fae4555eb87a330e4330b299c9
Iain Brookes <iain.brookes@outlook.com>
2021-02-26 00:04:33 +0000

    Added colour indication of selected language

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React from "react";
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All",
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage: selectedLanguage,
+    });
+  }
+
   render() {
     const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
@@ -8,7 +24,17 @@ export default class Popular extends React.Component {
       <ul className="flex-center">
         {languages.map((lang) => (
           <li key={lang}>
-            <button className="btn-clear nav-link">{lang}</button>
+            <button
+              className="btn-clear nav-link"
+              style={
+                lang === this.state.selectedLanguage
+                  ? { color: "rgb(255, 0, 255)" }
+                  : null
+              }
+              onClick={() => this.updateLanguage(lang)}
+            >
+              {lang}
+            </button>
           </li>
         ))}
       </ul>
